f82798beeee707a2b1147abe3fca53e388eed3cb
jqiu123 <jianweiqiu3@gmail.com>
2016-10-24 22:33:58 -0400

    Modify java code

--- Src/FibonacciUI.java
+++ Src/FibonacciUI.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class FibonacciUI {
+public class FibonacciUI 
+{
 
 	/**
 	 * Main entry point for the UI.
